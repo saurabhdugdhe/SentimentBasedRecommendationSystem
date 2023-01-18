399670c48035f30b9a8ff113e9c949d9d941191a
Saurabh Dugdhe <saurabhdugdhe18@gmail.com>
2023-01-18 14:26:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,27 @@
-from email import header
-from operator import index
 from flask import Flask, request, render_template
 from model import *
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def home():
     return render_template('index.html')
 
-
 @app.route('/predict', methods=['POST'])
 def prediction():
-    # get user from the html form
+
     user = request.form['userName']
-    # convert text to lowercase
+
     user = user.lower()
     items = GetSentimentRecommendations(user)
 
     if(not(items is None)):
         print(f"retrieving items....{len(items)}")
         print(items)
-        # data=[items.to_html(classes="table-striped table-hover", header="true",index=False)
+
         return render_template("index.html", column_names=items.columns.values, row_data=list(items.values.tolist()), zip=zip)
     else:
         return render_template("index.html", message="User Name doesn't exists, No product recommendations at this point of time!")
-
 
 if __name__ == '__main__':
     app.run()
